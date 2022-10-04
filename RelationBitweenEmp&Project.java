f4c6439aa6db38daaefc4060c67719c35b6cb415
Lakhan Gupta <99022559+lakhangupta1@users.noreply.github.com>
2022-10-05 02:24:32 +0530

    Create RelationBitweenEmp&Project.java

---
+++ RelationBitweenEmp&Project.java
@@ -0,0 +1,62 @@
+
+class Employee{
+    String Empid;
+    String Empname;
+    Address addr;
+     Employee(String name,String id,Address addrs){
+         Empid=id;
+         Empname=name;
+         addr=addrs;
+     }
+      
+
+    void show(){
+        System.out.println("Employee id is : "+ Empid);
+        System.out.println("Employe name is : "+Empname);
+        System.out.println("Employee Address is !");
+        System.out.println("State is : "+addr.state);
+        System.out.println("City is  : "+addr.City);
+        System.out.println("Location is : "+addr.Location);
+        System.out.println("--------------------------------------------------");
+
+
+     }
+
+}
+class Address{
+    String state;
+    String City;
+    String Location;
+    Address(String state1,String City1,String Location1){
+        state=state1;
+        City=City1;
+        Location=Location1;
+    }
+}
+public class OnetoOne {
+    public static void main(String[] args) {
+        Address ad1=new Address("Uttar Predesh ","Deoria"," Mahuapatan");
+        Employee emp1=new Employee("Lakhan", "01", ad1);
+
+
+        //second employee.
+        Address ad2=new Address("Uttar Predesh ","Deoria ","Mahuapatan");
+        Employee emp2=new Employee("Ajeet ", "02",ad2);
+        
+        
+        //third employee 
+         Employee emp3=new Employee("Shivanand ","03", ad2);
+          
+         //thre is first employeee
+         emp1.show();
+
+         //thie is second employeee 
+         emp2.show();
+         //this is third employee 
+         emp3.show();
+
+
+        
+    }
+    
+}
